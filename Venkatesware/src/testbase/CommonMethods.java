b3d597c2f530cded270db1f249b04bc0dc3cca46
Venkateshwara Rao Doijode <33220328+venkywarriors619@users.noreply.github.com>
2019-03-28 17:37:08 +0530

    Update CommonMethods.java

--- Venkatesware/src/testbase/CommonMethods.java
+++ Venkatesware/src/testbase/CommonMethods.java
@@ -61,6 +61,21 @@ public class CommonMethods
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].onmouseover()", element); 
 	}
+	public static Boolean isVisibleInViewport(WebElement element) {
+ // check element available in visible screen area
+ 	 return (Boolean)((JavascriptExecutor)driver).executeScript(
+      "var elem = arguments[0],                 " +
+      "  box = elem.getBoundingClientRect(),    " +
+      "  cx = box.left + box.width / 2,         " +
+      "  cy = box.top + box.height / 2,         " +
+      "  e = document.elementFromPoint(cx, cy); " +
+      "for (; e; e = e.parentElement) {         " +
+      "  if (e === elem)                        " +
+      "    return true;                         " +
+      "}                                        " +
+      "return false;                            "
+      , element);
+}
 	public void sendkeysJS(WebElement element,String value)
 	{
 	   element.clear();
